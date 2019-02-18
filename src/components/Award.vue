c3a5d0aca2c1668909ff8e2da62bc098bc5510e6
Teemu Haila <teemu.haila@playraven.com>
2019-02-18 18:24:39 +0200

    Added award photo

--- src/components/Award.vue
+++ src/components/Award.vue
@@ -2,12 +2,12 @@
 div#award-container.text-light.section-padding
   div.container
     div.d-flex.justify-content-center.mb-4
-      g-image(alt="IGDA Finland Lifetime Award" src="~/favicon.png" width="300")
-    p In 2018 I got a lifetime award for my contributions to the Finnish game development scene.
+      g-image(alt="IGDA Finland Lifetime Award" src="~/assets/award.png" width="300")
+    p.text-center In 2018 I got a lifetime award for my contributions to the Finnish game development scene.
 </template>
 
 <style lang="sass">
 #award-container
-  background-image: linear-gradient(rgb(93, 93, 93), rgb(0, 0, 0))
+  background-image: linear-gradient(rgb(30, 30, 30), rgb(5, 5, 5))
   padding-bottom: 1rem
 </style>
