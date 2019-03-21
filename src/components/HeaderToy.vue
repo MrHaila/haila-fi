6391e5d3730c88a807aacf23260330cb83a7b85d
Teemu Haila <teemu.haila@igda.fi>
2019-03-21 20:33:28 +0200

    Removed deprecated code

--- src/components/HeaderToy.vue
+++ src/components/HeaderToy.vue
@@ -5,21 +5,19 @@ div#toy-container
       p BA Interaction Design
       p MProf Games Development
       p Games Industry Entrepreneur & Executive
-  scene(@complete="complete")
+  scene()
     property(name="clearColor" color="#FFF")
     camera(v-model="camera" type="arcRotate")
-    entity(:position="[0, 0, 0]" :scaling="[2, 2, 2]")
+    entity(:scaling="[2, 2, 2]")
       animation(property="rotation.x" :duration="5" :end="Math.PI * 2")
       animation(property="rotation.y" :duration="5" :end="Math.PI * 2")
       animation(property="rotation.z" :duration="5" :end="Math.PI * 2")
       box
-        material(diffuse="#0C0C0C" reflection="#D8D8D8" :metallic="0.96" :roughness="0" :glossiness="0.5")
+        material(diffuse="#020202" reflection="#D8D8D8" :roughness="0")
           texture(type="environment" src="/assets/environment.dds")
 </template>
 
 <script>
-// import BABYLON from 'vue-babylonjs'
-
 export default {
   data: function () {
     return {
@@ -29,11 +27,6 @@ export default {
   watch: {
     camera () {
       this.camera.inputs.clear()
-    }
-  },
-  methods: {
-    complete (event) {
-      // Called after babylon scene is fully loaded
     }
   }
 }
