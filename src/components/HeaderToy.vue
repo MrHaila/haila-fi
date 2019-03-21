f0d96c27c027ab626ee8d83ab4c16b1c4e54dc16
Teemu Haila <teemu.haila@igda.fi>
2019-03-21 20:50:18 +0200

    Reverted babylon code. The battle is lost.

--- src/components/HeaderToy.vue
+++ src/components/HeaderToy.vue
@@ -5,32 +5,9 @@ div#toy-container
       p BA Interaction Design
       p MProf Games Development
       p Games Industry Entrepreneur & Executive
-  scene()
-    property(name="clearColor" color="#FFF")
-    camera(v-model="camera" type="arcRotate")
-    entity(:scaling="[2, 2, 2]")
-      animation(property="rotation.x" :duration="5" :end="Math.PI * 2")
-      animation(property="rotation.y" :duration="5" :end="Math.PI * 2")
-      animation(property="rotation.z" :duration="5" :end="Math.PI * 2")
-      box
-        material(diffuse="#020202" reflection="#D8D8D8" :roughness="0")
-          texture(type="environment" src="/assets/environment.dds")
+  div.d-flex.justify-content-center.mb-5
+    h4.mt-5 Work in progress...
 </template>
-
-<script>
-export default {
-  data: function () {
-    return {
-      camera: null
-    }
-  },
-  watch: {
-    camera () {
-      this.camera.inputs.clear()
-    }
-  }
-}
-</script>
 
 <style lang="sass">
 #toy-container
--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 // Global styles that get transpiled to a .css and then inlined by Critical CSS
 import '~/assets/custom-bootstrap.sass'
-import VueBabylon from 'vue-babylonjs'
 
 export default function (Vue, { router, head, isClient }) {
-  Vue.use(VueBabylon)
+
 }
