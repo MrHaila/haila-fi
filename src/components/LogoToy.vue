a7e32b801c1d8ca5b8e97f79a05711ef9919f477
Teemu Haila <teemu.haila@igda.fi>
2019-03-21 20:21:43 +0200

    Let's rotate a cube

--- src/components/LogoToy.vue
+++ src/components/LogoToy.vue
@@ -1,23 +1,55 @@
 <template lang="pug">
 div#toy-container
-  div.container
-    i.content-placeholder.d-flex.justify-content-center < Header content TBD >
-    div#bottom-list
+  div.text-overlay
+    div
       p BA Interaction Design
       p MProf Games Development
       p Games Industry Entrepreneur & Executive
+  scene(@complete="complete")
+    property(name="clearColor" color="#FFF")
+    camera(v-model="camera" type="arcRotate")
+    entity(:position="[0, 0, 0]" :scaling="[2, 2, 2]")
+      animation(property="rotation.x" :duration="5" :end="Math.PI * 2")
+      animation(property="rotation.y" :duration="5" :end="Math.PI * 2")
+      animation(property="rotation.z" :duration="5" :end="Math.PI * 2")
+      box
+        material(diffuse="#0C0C0C" reflection="#D8D8D8" :metallic="0.96" :roughness="0" :glossiness="0.5")
+          texture(type="environment" src="/assets/environment.dds")
 </template>
+
+<script>
+// import BABYLON from 'vue-babylonjs'
+
+export default {
+  data: function () {
+    return {
+      camera: null
+    }
+  },
+  watch: {
+    camera () {
+      this.camera.inputs.clear()
+    }
+  },
+  methods: {
+    complete (event) {
+      // Called after babylon scene is fully loaded
+    }
+  }
+}
+</script>
 
 <style lang="sass">
 #toy-container
   background-image: linear-gradient(rgb(238, 238, 238), rgb(255, 255, 255))
+  height: 25rem
 
-#bottom-list
-  margin-bottom: 0.5rem
-  p
-    margin-bottom: 0
+  .text-overlay
+    position: absolute
+    z-index: 1
+    top: 23rem
+    left: 10%
 
-.content-placeholder
-  padding-top: 130px
-  padding-bottom: 130px
+    p
+      margin-bottom: 0
 </style>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 // Global styles that get transpiled to a .css and then inlined by Critical CSS
 import '~/assets/custom-bootstrap.sass'
+import VueBabylon from 'vue-babylonjs'
 
 export default function (Vue, { router, head, isClient }) {
-
+  Vue.use(VueBabylon)
 }
