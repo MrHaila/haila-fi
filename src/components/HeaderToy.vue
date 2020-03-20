af10084dce01805ceb77216318e898df29a7e445
Teemu Haila <teemu.haila@playraven.com>
2020-03-20 22:15:27 +0200

    Fixed header rotations

--- src/components/HeaderToy.vue
+++ src/components/HeaderToy.vue
@@ -72,21 +72,17 @@ div#toy-container
 </style>
 
 <script>
-import {
-  Engine,
-  Scene,
-  Vector3,
-  Color3,
-  ArcRotateCamera,
-  CubeTexture,
-  SceneLoader,
-  PBRMetallicRoughnessMaterial,
-  Animation,
-  QuinticEase,
-  CircleEase,
-  EasingFunction,
-  Quaternion
-} from '@babylonjs/core'
+import { Engine } from '@babylonjs/core/Engines/engine'
+import { Scene } from '@babylonjs/core/scene'
+import { Vector3, Quaternion, Color3 } from '@babylonjs/core/Maths/math'
+import { ArcRotateCamera } from '@babylonjs/core/Cameras/arcRotateCamera'
+import { Animation, QuinticEase, CircleEase, EasingFunction } from '@babylonjs/core/Animations/index'
+import { CubeTexture } from '@babylonjs/core/Materials/Textures/cubeTexture'
+import '@babylonjs/core/Materials/Textures/Loaders/ddsTextureLoader'
+import '@babylonjs/core/Misc/dds'
+import '@babylonjs/core/Helpers/sceneHelpers'
+import { SceneLoader } from '@babylonjs/core/Loading/sceneLoader'
+import { PBRMetallicRoughnessMaterial } from '@babylonjs/core/Materials/PBR/pbrMetallicRoughnessMaterial'
 import '@babylonjs/loaders'
 // import '@babylonjs/core/Debug/debugLayer'
 // import '@babylonjs/inspector'
@@ -170,21 +166,21 @@ export default {
     const environmentTexture = CubeTexture.CreateFromPrefilteredData('/assets/environment_32.env', scene)
     environmentTexture.gammaSpace = false
     environmentTexture.rotationY = 2.8
-    let skybox = scene.createDefaultSkybox(environmentTexture, true, 100, 0.4, true)
+    const skybox = scene.createDefaultSkybox(environmentTexture, true, 100, 0.4, true)
     skybox.material.alpha = 0.05
     scene.autoClear = false
     scene.autoClearDepthAndStencil = false
 
     // Step 2: Camera
-    let camera = new ArcRotateCamera('camera', 0, 0, 2, new Vector3(0, -1.5, 10), scene)
+    const camera = new ArcRotateCamera('camera', 0, 0, 2, new Vector3(0, -1.5, 10), scene)
     camera.setTarget(Vector3.Zero())
 
-    let cameraAnimation = new Animation('cameraAnimation', 'alpha', 30, Animation.ANIMATIONTYPE_FLOAT, Animation.ANIMATIONLOOPMODE_RELATIVE)
+    const cameraAnimation = new Animation('cameraAnimation', 'alpha', 30, Animation.ANIMATIONTYPE_FLOAT, Animation.ANIMATIONLOOPMODE_RELATIVE)
     cameraAnimation.setKeys([
       { frame: 0, value: Math.PI * 0.5 },
       { frame: 300, value: Math.PI + Math.PI * 0.5 }
     ])
-    let easingFunction = new QuinticEase()
+    const easingFunction = new QuinticEase()
     easingFunction.setEasingMode(EasingFunction.EASINGMODE_EASEINOUT)
     cameraAnimation.setEasingFunction(easingFunction)
     camera.animations.push(cameraAnimation)
@@ -193,12 +189,12 @@ export default {
     // Step 3: Models, materials and animations
     SceneLoader.ImportMeshAsync(null, '/assets/', 'haila.glb', scene).then((result) => {
       // Character fly-in rotation animation
-      let characterRotation = new Animation('characterRotation', 'rotationQuaternion', 30, Animation.ANIMATIONTYPE_QUATERNION, Animation.ANIMATIONLOOPMODE_CONSTANT)
+      const characterRotation = new Animation('characterRotation', 'rotationQuaternion', 30, Animation.ANIMATIONTYPE_QUATERNION, Animation.ANIMATIONLOOPMODE_CONSTANT)
       characterRotation.setKeys([
         { frame: 0, value: new Quaternion(0, 0, 0, -1) },
         { frame: 20, value: new Quaternion(0.707, 0, 0, 0.707) }
       ])
-      let easingFunction = new CircleEase()
+      const easingFunction = new CircleEase()
       easingFunction.setEasingMode(EasingFunction.EASINGMODE_EASEOUT)
       characterRotation.setEasingFunction(easingFunction)
 
@@ -212,7 +208,7 @@ export default {
         result.meshes[i].material.sideOrientation = 0
 
         // Create per-character fly-in animation
-        let characterPosition = new Animation('characterPosition', 'position', 30, Animation.ANIMATIONTYPE_VECTOR3, Animation.ANIMATIONLOOPMODE_CONSTANT)
+        const characterPosition = new Animation('characterPosition', 'position', 30, Animation.ANIMATIONTYPE_VECTOR3, Animation.ANIMATIONLOOPMODE_CONSTANT)
         characterPosition.setKeys([
           { frame: 0, value: new Vector3(i * 2 - 2, -20, -30) },
           { frame: 20, value: result.meshes[i].position }
@@ -223,7 +219,7 @@ export default {
         // Setup animations
         result.meshes[i].animations.push(characterRotation)
         result.meshes[i].animations.push(characterPosition)
-        setTimeout(() => scene.beginAnimation(result.meshes[i], 0, 20, true), i * 70)
+        setTimeout(() => scene.beginAnimation(result.meshes[i], 0, 20, true), i * 70, false)
       }
 
       // Delayed start to camera animation
@@ -249,16 +245,18 @@ export default {
     scene.onPointerPick = (evt, pickInfo) => {
       if (pickInfo.hit) {
         // Setup color & spin animation
-        let characterSpin = new Animation('characterSpin', 'rotation.z', 30, Animation.ANIMATIONTYPE_FLOAT, Animation.ANIMATIONLOOPMODE_RELATIVE)
-        characterSpin.setKeys([
-          { frame: 0, value: 0 },
-          { frame: 20, value: 0.105 }
+        const characterRotation = new Animation('characterRotation', 'rotationQuaternion', 30, Animation.ANIMATIONTYPE_QUATERNION, Animation.ANIMATIONLOOPMODE_CONSTANT)
+        const random = (Math.random() - 0.5) * 0.15
+        characterRotation.setKeys([
+          { frame: 0, value: pickInfo.pickedMesh.rotationQuaternion },
+          { frame: 20, value: new Quaternion(0.707 + random, random, random, 0.707 + random) }
         ])
-        let easingFunction = new CircleEase()
-        easingFunction.setEasingMode(EasingFunction.EASINGMODE_EASEOUT)
-        characterSpin.setEasingFunction(easingFunction)
 
-        let characterColor = new Animation('characterColor', 'material.emissiveColor', 30, Animation.ANIMATIONTYPE_COLOR3, Animation.ANIMATIONLOOPMODE_CONSTANT)
+        const easingFunction = new CircleEase()
+        easingFunction.setEasingMode(EasingFunction.EASINGMODE_EASEOUT)
+        characterRotation.setEasingFunction(easingFunction)
+
+        const characterColor = new Animation('characterColor', 'material.emissiveColor', 30, Animation.ANIMATIONTYPE_COLOR3, Animation.ANIMATIONLOOPMODE_CONSTANT)
         characterColor.setKeys([
           { frame: 0, value: new Color3(1, 1, 1) },
           { frame: 20, value: new Color3(0, 0, 0) }
@@ -266,7 +264,7 @@ export default {
 
         pickInfo.pickedMesh.animations = []
         pickInfo.pickedMesh.animations.push(characterColor)
-        pickInfo.pickedMesh.animations.push(characterSpin)
+        pickInfo.pickedMesh.animations.push(characterRotation)
 
         // Actually do stuff
         scene.beginAnimation(pickInfo.pickedMesh, 0, 20, false)
@@ -274,12 +272,12 @@ export default {
 
         // Update HUD
         this.localClicks[pickInfo.pickedMesh.name]++
-        let r = Math.floor(pickInfo.pickedMesh.material.baseColor.r * 256)
-        let g = Math.floor(pickInfo.pickedMesh.material.baseColor.g * 256)
-        let b = Math.floor(pickInfo.pickedMesh.material.baseColor.b * 256)
-        let r2 = Math.round(Math.min(Math.max(0, r + (r * 0.2))))
-        let g2 = Math.round(Math.min(Math.max(0, g + (g * 0.2))))
-        let b2 = Math.round(Math.min(Math.max(0, b + (b * 0.2))))
+        const r = Math.floor(pickInfo.pickedMesh.material.baseColor.r * 256)
+        const g = Math.floor(pickInfo.pickedMesh.material.baseColor.g * 256)
+        const b = Math.floor(pickInfo.pickedMesh.material.baseColor.b * 256)
+        const r2 = Math.round(Math.min(Math.max(0, r + (r * 0.2))))
+        const g2 = Math.round(Math.min(Math.max(0, g + (g * 0.2))))
+        const b2 = Math.round(Math.min(Math.max(0, b + (b * 0.2))))
         this.scoreStyle[pickInfo.pickedMesh.name]['background-image'] = 'linear-gradient(rgb(' + r2 + ',' + g2 + ',' + b2 + '), rgb(' + r + ',' + g + ',' + b + ')'
         this.scoreStyle[pickInfo.pickedMesh.name].color = (r * 0.299 + g * 0.587 + b * 0.114) > 186 ? 'black' : 'white'
 
@@ -303,7 +301,7 @@ export default {
     })
     this.firestore = Firebase.firestore()
     this.firestore.collection('logo').onSnapshot(snapshot => {
-      for (let doc of snapshot.docs) {
+      for (const doc of snapshot.docs) {
         this.clicks[doc.id] = doc.data().count
       }
       this.$emit('updateclicks', this.clicks)
