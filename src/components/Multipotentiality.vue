e77c065f4131dc2e894a5e49e41ee9e52cc5b6ff
Teemu Haila <teemu.haila@playraven.com>
2020-03-21 16:20:07 +0200

    Fixed svg overflow on mobile

--- src/components/Multipotentiality.vue
+++ src/components/Multipotentiality.vue
@@ -8,55 +8,55 @@ div#potentiality-container.text-light
     p.ml-3.outline-on-mobile 2. Sleep
     p.ml-3
       span.outline-on-mobile 3.
-      svg.overlay(height="100%" width="100%" stroke-width="5" stroke="red")
+      svg.overlay(height="100%" width="125" stroke-width="5" stroke="red")
         line(x1="6" x2="65" y1="22" y2="22")
       span.outline-on-mobile  Code
       span.ml-4
-        svg.overlay(height="100%" width="100%" stroke-width="5" stroke="red")
+        svg.overlay(height="100%" width="125" stroke-width="5" stroke="red")
           line(x1="1" x2="77" y1="21" y2="21")
         span.outline-on-mobile Design
     p.ml-5
-      svg.overlay(height="100%" width="100%" stroke-width="5" stroke="red")
+      svg.overlay(height="100%" width="125" stroke-width="5" stroke="red")
         line(x1="3" x2="125" y1="18" y2="18")
       span.outline-on-mobile Photograph
     p.ml-4
-      svg.overlay(height="100%" width="100%" stroke-width="5" stroke="red")
+      svg.overlay(height="100%" width="125" stroke-width="5" stroke="red")
         line(x1="0" x2="39" y1="20" y2="20")
       span.outline-on-mobile Play
       span.ml-4
-        svg.overlay(height="100%" width="100%" stroke-width="5" stroke="red")
+        svg.overlay(height="100%" width="125" stroke-width="5" stroke="red")
           line(x1="3" x2="85" y1="22" y2="22")
         span.outline-on-mobile Exercise
     p.ml-5
-      svg.overlay(height="100%" width="100%" stroke-width="5" stroke="red")
+      svg.overlay(height="100%" width="125" stroke-width="5" stroke="red")
         line(x1="0" x2="50" y1="18" y2="18")
       span.outline-on-mobile Cook
       span.ml-2
-        svg.overlay(height="100%" width="100%" stroke-width="5" stroke="red")
+        svg.overlay(height="100%" width="125" stroke-width="5" stroke="red")
           line(x1="1" x2="35" y1="19" y2="19")
         span.outline-on-mobile DIY
     p.ml-3
-      svg.overlay(height="100%" width="100%" stroke-width="5" stroke="red")
+      svg.overlay(height="100%" width="125" stroke-width="5" stroke="red")
         line(x1="0" x2="47" y1="22" y2="22")
       span.outline-on-mobile Read
       span.ml-3
-        svg.overlay(height="100%" width="100%" stroke-width="5" stroke="red")
+        svg.overlay(height="100%" width="125" stroke-width="5" stroke="red")
           line(x1="1" x2="110" y1="19" y2="21")
         span.outline-on-mobile Evangelise
     p.ml-4
-      svg.overlay(height="100%" width="100%" stroke-width="5" stroke="red")
+      svg.overlay(height="100%" width="125" stroke-width="5" stroke="red")
         line(x1="4" x2="58" y1="19" y2="19")
       span.outline-on-mobile Teach
       span.ml-4
-        svg.overlay(height="100%" width="100%" stroke-width="5" stroke="red")
+        svg.overlay(height="100%" width="125" stroke-width="5" stroke="red")
           line(x1="1" x2="60" y1="21" y2="21")
         span.outline-on-mobile Learn
     p.ml-2
-      svg.overlay(height="100%" width="100%" stroke-width="5" stroke="red")
+      svg.overlay(height="100%" width="125" stroke-width="5" stroke="red")
         line(x1="3" x2="54" y1="22" y2="22")
       span.outline-on-mobile Travel
       span.ml-4
-        svg.overlay(height="100%" width="100%" stroke-width="5" stroke="red")
+        svg.overlay(height="100%" width="125" stroke-width="5" stroke="red")
           line(x1="1" x2="69" y1="20" y2="20")
         span.outline-on-mobile Create
 
