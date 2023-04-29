e0a55bddcad3687ac8db8d8f9e062b23d640c937
Teemu Haila <teemu.haila@metaplay.io>
2023-04-29 11:58:09 +0300

    Experimenting with srcset. WIP.

--- src/sections/SeriousBusinessBlock.vue
+++ src/sections/SeriousBusinessBlock.vue
@@ -17,7 +17,8 @@ div(class="px-4 py-8 sm:p-10 container mx-auto space-y-8 bg-gradient-to-b from-w
     template(#default="{ item, index }")
       FadeIn(class="bg-neutral-100 rounded-xl shadow-lg" :delay="index * 10")
         img(
-          :src="item.image"
+          :src="`/assets/cards/${item.image}.jpg`"
+          :srcset="`/assets/cards/${item.image}@1x.webp 1x, /assets/cards/${item.image}@2x.webp 2x`"
           :alt="item.imageAlt"
           class="rounded-t-xl"
           )
@@ -35,98 +36,98 @@ const items = [
     title: 'Playraven',
     subtitle: 'Games Company',
     description: 'I co-founded a mobile games company focused on free-to-play strategy games. After 6 years, $10M raised capital and 5 shipped games we were aquired by Rovio.',
-    image: '/assets/card-playraven.jpg',
+    image: 'card-playraven',
     imageAlt: 'Playraven logo',
   },
   {
     title: 'Eve: War of Ascension',
     subtitle: 'Mobile Game',
     description: 'An ambitious take on 4X style MMOs on mobile in collaboration with CCP. The company was aquired by Rovio during soft launch and the project was rebooted under a different brand.',
-    image: '/assets/card-eve.png',
+    image: 'card-eve',
     imageAlt: 'Eve: War of Ascension key art',
   },
   {
     title: 'Robocide',
     subtitle: 'Mobile Game',
     description: 'Micro RTS where you direct a swarm of hundreds of robots with a single tap. Top 1 strategy in 41 and top 5 in 104 countries.',
-    image: '/assets/card-robocide.jpg',
+    image: 'card-robocide',
     imageAlt: 'Robocide key art',
   },
   {
     title: 'Spymaster (reboot)',
     subtitle: 'Mobile Game',
     description: 'A new take on spy management with a collection of hundreds of unique agents and various agency (clan/guild) activities to complete.',
-    image: '/assets/card-spymaster2.png',
+    image: 'card-spymaster2',
     imageAlt: 'Spymaster key art',
   },
   {
     title: 'Winterstate',
     subtitle: 'Mobile Game',
     description: 'Cars with guns in frozen USA. Vehicle RTS with a campaign and async multiplayer. Top 5 strategy in 133 countries.',
-    image: '/assets/card-winterstate.jpg',
+    image: 'card-winterstate',
     imageAlt: 'Winterstate key art',
   },
   {
     title: 'Spymaster',
     subtitle: 'Mobile Game',
     description: 'A spy management game with a collection of hundreds of unique agents and various agency (clan/guild) activities to complete.',
-    image: '/assets/card-spymaster1.jpg',
+    image: 'card-spymaster1',
     imageAlt: 'Spymaster key art',
   },
   {
     title: 'Bubble Island iOS',
     subtitle: 'Mobile Game',
     description: 'Mobile SKU of the most successful bubble shooter at the time. Game of the week in App Store, millions of MAU, etc.',
-    image: '/assets/card-bi.png',
+    image: 'card-bi',
     imageAlt: 'Bubble Island key art',
   },
   {
     title: 'Diamond Dash iOS',
     subtitle: 'Mobile Game',
     description: 'Mobile SKU of Wooga\'s biggest arcade game at the time with over 10M MAU. We were the first game to ship with Facebook Connect and other bleeding edge social features.',
-    image: '/assets/card-ddios.png',
+    image: 'card-ddios',
     imageAlt: 'Diamond Dash key art',
   },
   {
     title: 'Score',
     subtitle: 'Community',
     description: 'I founded Finland\'s first game development club in Tampere University of Applied Sciences with a goal of improving the available games education.',
-    image: '/assets/card-score.png',
+    image: 'card-score',
     imageAlt: 'Score logo',
   },
   {
     title: 'Emmi',
     subtitle: 'Fullstack Web App',
     description: 'IGDA Finland\'s tech backbone that handles all events and memberships across thousands of people every year. Open sourced under MIT.',
-    image: '/assets/card-emmi.png',
+    image: 'card-emmi',
     imageAlt: 'Emmi screenshot',
   },
   {
     title: 'High Score',
     subtitle: 'Company',
     description: 'I founded a series of non-profit companies on top of game development clubs as a part of building a stronger career path for aspiring developers in Finland.',
-    image: '/assets/card-highscore.png',
+    image: 'card-highscore',
     imageAlt: 'High Score screenshot',
   },
   {
     title: 'Fingamers',
     subtitle: 'Community',
     description: 'I founded Finland\'s first online gaming community provider. We crowdfunded and hosted some of the biggest MMO communities at the time.',
-    image: '/assets/card-fingamers.png',
+    image: 'card-fingamers',
     imageAlt: 'Fingamers screenshot',
   },
   {
     title: 'Runepoli',
     subtitle: 'Community',
     description: 'I founded Finland\'s biggest Runescape community that united the various trade forums under a single banner. It survives to this day and is officially supported by Jagex.',
-    image: '/assets/card-runepoli.png',
+    image: 'card-runepoli',
     imageAlt: 'Runepoli screenshot',
   },
   {
     title: 'Refugee Work',
     subtitle: 'Charity',
     description: 'I am a volunteer photographer in a program that organises Lapland wilderness tours for refugees to help with societal integration.',
-    image: '/assets/card-refugees.jpg',
+    image: 'card-refugees',
     imageAlt: 'Photo of refugees climbing Saana',
   },
 ]
