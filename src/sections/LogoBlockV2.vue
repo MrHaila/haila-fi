ffa4b9294495653b17f8d3659a7ddd8d858c1abe
Teemu Haila <teemu.haila@gmail.com>
2024-09-15 10:08:22 +0300

    Basic setup for rendering, animation and inputs

--- src/sections/LogoBlockV2.vue
+++ src/sections/LogoBlockV2.vue
@@ -1,5 +1,8 @@
 <template lang="pug">
-div(class="relative bg-gradient-to-t from-neutral-100 to-white")
+div(
+  class="relative bg-gradient-to-t from-neutral-100 to-white"
+  :class="{ 'cursor-pointer': !!hoveredCharacter }"
+  )
   p(
     v-show="!isEngineLoaded"
     class="absolute top-40 z-10 w-full text-center text-xl text-neutral-600"
@@ -57,45 +60,22 @@ import {
   PerspectiveCamera,
   WebGLRenderer,
   BoxGeometry,
-  MeshBasicMaterial,
   Mesh,
-  CubeTextureLoader,
   AmbientLight,
   DirectionalLight,
   MeshStandardMaterial,
   Color,
+  Raycaster,
+  Vector2,
+  DataTexture,
+  EquirectangularReflectionMapping,
 } from 'three'
 import WebGL from 'three/addons/capabilities/WebGL.js'
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js'
+import { RGBELoader } from 'three/addons/loaders/RGBELoader.js'
 import { ref, computed, onMounted } from 'vue'
 
 // import { useSupabase } from '../useSupabase'
-
-const isEngineLoaded = ref(false)
-const emits = defineEmits(['loaded'])
-
-const scoreStyle = ref({
-  h1: {
-    'background-image': 'linear-gradient(rgb(238, 238, 238), rgb(255, 255, 255))',
-    color: 'black',
-  },
-  a2: {
-    'background-image': 'linear-gradient(rgb(238, 238, 238), rgb(255, 255, 255))',
-    color: 'black',
-  },
-  i3: {
-    'background-image': 'linear-gradient(rgb(238, 238, 238), rgb(255, 255, 255))',
-    color: 'black',
-  },
-  l4: {
-    'background-image': 'linear-gradient(rgb(238, 238, 238), rgb(255, 255, 255))',
-    color: 'black',
-  },
-  a5: {
-    'background-image': 'linear-gradient(rgb(238, 238, 238), rgb(255, 255, 255))',
-    color: 'black',
-  },
-})
 
 interface Clicks {
   h1: number
@@ -134,43 +114,90 @@ const feedback = computed(() => {
   else return ''
 })
 
+// const { totalClicks, h1, a2, i3, l4, a5, incrementClick } = useSupabase()
+
+// 3D Shenanigans -----------------------------------------------------------------------------------------------------
 const headerCanvas = ref<HTMLCanvasElement>()
 const isWebGlAvailable = WebGL.isWebGL2Available()
+const isEngineLoaded = ref(false)
 
-// const { totalClicks, h1, a2, i3, l4, a5, incrementClick } = useSupabase()
+const scene = new Scene()
+
+const defaultMaterial = new MeshStandardMaterial({ color: new Color(0.7, 0.7, 0.7), roughness: 0.25, metalness: 0.2 })
+const hoverMaterial = new MeshStandardMaterial({ color: 0xff0000 })
+const clickMaterial = new MeshStandardMaterial({ color: 0x0000ff })
+
+const raycaster = new Raycaster()
+const mouse = new Vector2()
+let hoveredMesh: Mesh | null = null
+const hoveredCharacter = ref<'h1' | 'a2' | 'i3' | 'l4' | 'a5'>()
+
+function onClick(): void {
+  if (!hoveredMesh) return
+  hoveredMesh.material = clickMaterial
+}
+
+function onHover(mesh: Mesh): void {
+  if (hoveredMesh !== mesh) {
+    // If a new object is being hovered, revert the old object's material.
+    if (hoveredMesh) hoveredMesh.material = defaultMaterial
+
+    // Set the new hovered object's material.
+    mesh.material = hoverMaterial
+    hoveredMesh = mesh
+    hoveredCharacter.value = mesh.name as 'h1' | 'a2' | 'i3' | 'l4' | 'a5'
+  }
+}
+
+function onNoHover(): void {
+  if (hoveredMesh) hoveredMesh.material = defaultMaterial
+  hoveredMesh = null
+  hoveredCharacter.value = undefined
+}
+
+function update(sceneTime: number, deltaTime: number): void {
+  const z = Math.sin(sceneTime * 0.0005) / 12
+  const x = Math.cos(sceneTime * 0.0009) / 5
+
+  // Sine wave rotation
+  scene.rotation.z = z
+  scene.rotation.x = x
+}
 
 onMounted(async () => {
   if (!isWebGlAvailable) return
   if (!headerCanvas.value) throw new Error('Header canvas not found')
 
-  // Step 1: Scene + camera
-  const scene = new Scene()
+  // Step 1: Camera
   const camera = new PerspectiveCamera(35, headerCanvas.value.clientWidth / headerCanvas.value.clientHeight, 0.1, 1000)
   camera.position.z = 12
 
-  const ambientLight = new AmbientLight(0xffffff, 1)
-  scene.add(ambientLight)
+  // Step 2: Environment
+  const hdr = '/assets/3d/cloudy_128.hdr'
+  new RGBELoader().load(hdr, (texture) => {
+    texture.mapping = EquirectangularReflectionMapping
+    scene.environment = texture
+    scene.backgroundBlurriness = 0.2
+  })
 
-  // const directionalLight = new DirectionalLight(0xffffff, 1)
-  // directionalLight.position.set(5, 10, 7.5)
-  //scene.add(directionalLight)
+  // Step 3: Models, materials and animations
+  const characterObjects: Mesh[] = []
 
-  // Step 2: Environment (env texture from Babylon Playground)
   const gltfLoader = new GLTFLoader()
   gltfLoader.load(
-    '/assets/haila.glb',
+    '/assets/3d/haila.glb',
     (gltf) => {
       const gltfScene = gltf.scene
 
-      gltfScene.traverse((child) => {
-        if (child instanceof Mesh) {
-          // Create a new material for each mesh
-          child.material = new MeshStandardMaterial({
-            // color: new Color(0.05, 0.05, 0.05),
-            color: new Color(1, 1, 1),
-            roughness: 0.25,
-            metalness: 0.2,
-          })
+      // Traverse the scene and apply the default material to all meshes.
+      gltfScene.traverse((obj) => {
+        if (obj instanceof Mesh) {
+          obj.material = defaultMaterial
+          obj.geometry.computeVertexNormals()
+
+          // Store the mesh in the characterObject array.
+          // eslint-disable-next-line @typescript-eslint/no-unsafe-argument
+          characterObjects.push(obj)
         }
       })
 
@@ -183,46 +210,56 @@ onMounted(async () => {
     }
   )
 
-  const textureLoader = new CubeTextureLoader()
-  const envTexture = textureLoader.load(['/assets/environment_32.env'])
-
-  scene.environment = envTexture
-
-  const skyboxMaterial = new MeshBasicMaterial({ envMap: envTexture })
-  const skyboxGeometry = new BoxGeometry(100, 100, 100)
-  const skybox = new Mesh(skyboxGeometry, skyboxMaterial)
-  scene.add(skybox)
-
-  // Step 3: Models, materials and animations
-
   // Step 4: Inputs
+  window.addEventListener('mousemove', (event) => {
+    // Calculate mouse position inside the canvas.
+    if (!headerCanvas.value) return
+    mouse.x = (event.clientX / headerCanvas.value.clientWidth) * 2 - 1
+    mouse.y = -(event.clientY / headerCanvas.value.clientHeight) * 2 + 1
+  })
+
+  window.addEventListener('click', onClick)
 
   // Step 5: Renderer
   const renderer = new WebGLRenderer({
     canvas: headerCanvas.value,
     antialias: true,
+    alpha: true,
   })
 
   renderer.setSize(headerCanvas.value.clientWidth, headerCanvas.value.clientHeight)
 
+  // Handle window resizing
+  // window.addEventListener('resize', () => {
+  //   if (!headerCanvas.value) return
+  //   camera.aspect = headerCanvas.value.clientWidth / headerCanvas.value.clientHeight
+  //   camera.updateProjectionMatrix()
+  //   renderer.setSize(headerCanvas.value.clientWidth, headerCanvas.value.clientHeight)
+  // })
+
+  // Step 6: Update loop
+  const sceneTimeStart = Date.now()
+  let lastTime = Date.now()
   renderer.setAnimationLoop(() => {
+    // Animate the scene.
     const time = Date.now()
-    const z = Math.sin(time * 0.0005) / 6
-    const x = Math.cos(time * 0.0009) / 4
+    update(time - sceneTimeStart, time - lastTime)
+    lastTime = time
 
-    // Sine wave rotation
-    scene.rotation.z = z
-    scene.rotation.x = x
+    // Input handling.
+    raycaster.setFromCamera(mouse, camera)
+    const intersects = raycaster.intersectObjects(characterObjects)
+    if (intersects.length > 0) {
+      const intersectedMesh = intersects[0].object as Mesh
+      onHover(intersectedMesh)
+    } else {
+      onNoHover()
+    }
 
+    // Render the scene.
     renderer.render(scene, camera)
   })
 
   isEngineLoaded.value = true
-  emits('loaded')
-
-  // Handle window resizing
-  window.addEventListener('resize', () => {
-    // engine.resize()
-  })
 })
 </script>
